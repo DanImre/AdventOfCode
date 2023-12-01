29bffd8da5b6e5d5f53e03f6202d196c409a143a
DanImre <dan.imre.17f@gmail.com>
2023-12-01 08:04:03 +0100

    Day 1 - Both Stars

---
+++ AdventOfCode2023/AdventOfCode2023/Day1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    public class Day1
+    {
+        public Day1()
+        {
+
+        }
+
+        public int PartOne() //55208
+        {
+            return File.ReadAllLines("day1.txt").Select(kk => kk.Where(zz => char.IsDigit(zz))).Sum(kk => int.Parse(kk.First().ToString() + kk.Last().ToString()));
+        }
+
+        public long PartTwo() //54578
+        {
+            string[] s = File.ReadAllLines("day1.txt");
+            Dictionary<string, int> dic = new Dictionary<string, int>()
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 }
+            };
+
+            int solution = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //forwards
+                for (int j = 0; j < s[i].Length; j++)
+                {
+                    if (char.IsDigit(s[i][j]))
+                    {
+                        solution += int.Parse(s[i][j].ToString()) * 10;
+                        break;
+                    }
+                    bool found = false;
+                    string curr = "";
+                    for (int z = 0; z < 5 && j + z < s[i].Length; z++)
+                    {
+                        curr += s[i][j + z];
+                        if (z < 2)
+                            continue;
+                        if (dic.ContainsKey(curr))
+                        {
+                            solution += dic[curr] * 10;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found)
+                        break;
+                }
+
+                //backwards
+                for (int j = s[i].Length - 1; j >= 0; j--)
+                {
+                    if (char.IsDigit(s[i][j]))
+                    {
+                        solution += int.Parse(s[i][j].ToString());
+                        break;
+                    }
+                    bool found = false;
+                    string curr = "";
+                    for (int z = 0; z < 5 && j + z < s[i].Length; z++)
+                    {
+                        curr += s[i][j + z];
+                        if (z < 2)
+                            continue;
+                        if (dic.ContainsKey(curr))
+                        {
+                            solution += dic[curr];
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found)
+                        break;
+                }
+            }
+
+            return solution;
+        }
+    }
+}
---
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WHich day and part would you like to run? [1-25]");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Day1 d = new Day1();
+                    Console.WriteLine("Part 1 or Part 2? [1,2]");
+                    if (Console.ReadLine() == "1")
+                        Console.WriteLine(d.PartOne());
+                    else
+                        Console.WriteLine(d.PartTwo());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
