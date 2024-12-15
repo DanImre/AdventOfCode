a6ed27527d0b7c50894ed0bcec65bd37cb6a8a46
Dán Imre <dan.imre.17f@gmail.com>
2024-12-15 12:20:02 +0100

    Advent of Code 2024 - Day 14 Both Stars

---
+++ AdventOfCode2024/AdventOfCode2024/Day15.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    public class Day15 : IDay
+    {
+        //public string InputPath { get; set; } = "day15Input.txt";
+        public string InputPath { get; set; } = "day15TestInput.txt";
+        //public string InputPath { get; set; } = "day15TestInput2.txt";
+
+        public void PartOne()
+        {
+            string[] s = File.ReadAllLines(InputPath);
+
+            List<List<char>> map = new();
+            Queue<char> instructions = new();
+
+            using (StreamReader f = new StreamReader(InputPath))
+            {
+                while (!f.EndOfStream
+                    && f.ReadLine() is string line && line != "")
+                    map.Add(line.ToCharArray().ToList());
+
+                while (!f.EndOfStream)
+                    foreach (var item in f.ReadLine()!)
+                        instructions.Enqueue(item);
+            }
+
+            (int y, int x) robotPos = (0, 0);
+
+            for (int i = 0; i < map.Count; i++)
+                for (int j = 0; j < map[i].Count; j++)
+                    if (map[i][j] == '@')
+                    {
+                        robotPos = (i, j);
+                        break;
+                    }
+
+            Console.WriteLine(robotPos);
+
+        }
+
+        public void PartTwo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2024/DataStructures/RestroomRobot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class RestroomRobot
+    {
+        public (int x, int y) Pos { get; set; } = (0, 0);
+        public (int x, int y) Velocity { get; set; } = (0, 0);
+        public RestroomRobot((int x, int y) pos, (int x, int y) velocity)
+        {
+            Pos = pos;
+            Velocity = velocity;
+        }
+    }
+}
