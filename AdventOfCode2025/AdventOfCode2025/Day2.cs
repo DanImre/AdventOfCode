047de7c0bd2af33e8fcd83d4e966f558db2f8d7b
Dán Imre <dan.imre.17f@gmail.com>
2025-12-02 18:00:54 +0100

    2025 Day 2 - Second star

--- AdventOfCode2025/AdventOfCode2025/Day2.cs
+++ AdventOfCode2025/AdventOfCode2025/Day2.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2025
+﻿namespace AdventOfCode2025
 {
     public class Day2 : IDay
     {
@@ -44,11 +34,20 @@ namespace AdventOfCode2025
                     if (i % 2 != 0)
                         continue;
 
-                    long startingNumber = Math.Max((long)Math.Pow(10, i / 2 - 1), startNum);
-                    long endingNumber = Math.Min((long)Math.Pow(10, i / 2), endNum);
-                    for (long half = startingNumber; half < endingNumber; half++)
+                    int digitCount = i / 2 - 1;
+                    long startingNumber = (long)Math.Pow(10, digitCount);
+                    long endingNumber = (long)Math.Pow(10, digitCount + 1);
+
+                    for (long half = startingNumber; half <= endingNumber; half++)
                     {
                         long number = long.Parse(half.ToString() + half.ToString());
+
+                        if (number < startNum)
+                            continue;
+
+                        if (number > endNum)
+                            break;
+
                         solution += number;
                     }
                 }
@@ -60,7 +59,68 @@ namespace AdventOfCode2025
 
         public void PartTwo()
         {
+            List<string[]> ranges = File.ReadAllLines(InputPath)
+                .SelectMany(x => x.Split(','))
+                .Select(x => x.Split('-')
+                    .Select(x => x.Trim())
+                    .ToArray())
+                .ToList();
 
+            long solution = 0;
+
+            HashSet<long> foundNumbers = [];
+            foreach (var item in ranges)
+            {
+                int minLength = Math.Min(item[0].Length, item[1].Length);
+                int maxLength = Math.Max(item[0].Length, item[1].Length);
+
+                long startNum = long.Parse(item[0]);
+                long endNum = long.Parse(item[1]);
+
+                // Invalid range
+                if (long.Parse(item[0]) > long.Parse(item[1]))
+                    continue;
+
+                // For ranges that have multiple even length number option like 12 - 1234 (has 2 and 4)
+                for (int i = minLength; i <= maxLength; i++)
+                {
+                    // Getting possible sequence lengths
+                    List<int> ps = PossibleRepeatingLengths(i);
+                    foreach (var slen in ps)
+                    {
+                        int digitCount = i / slen - 1;
+                        long startingNumber = (long)Math.Pow(10, digitCount);
+                        long endingNumber = (long)Math.Pow(10, digitCount + 1);
+                        for (long half = startingNumber; half < endingNumber; half++)
+                        {
+                            long number = long.Parse(string.Concat(Enumerable.Repeat(half.ToString(), slen)));
+
+                            if (number < startNum)
+                                continue;
+
+                            if (number > endNum)
+                                break;
+
+                            if (foundNumbers.Add(number))
+                                solution += number;
+                        }
+                    }
+                }
+            }
+
+            // 34421651192
+            Console.WriteLine($"The sum of all the invalid IDs (using the new rules) is {solution}");
+        }
+
+        private List<int> PossibleRepeatingLengths(int length)
+        {
+            List<int> solution = [];
+
+            for (int i = 2; i <= length; i++)
+                if (length % i == 0)
+                    solution.Add(i);
+
+            return solution;
         }
     }
 }
