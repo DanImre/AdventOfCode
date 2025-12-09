bb3849501441757af17db2a200ef4b24dd6f6436
Dán Imre <dan.imre.17f@gmail.com>
2025-12-09 22:28:24 +0100

    2025 Day 9 - Both stars

---
+++ AdventOfCode2025/AdventOfCode2025/Day9.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using DataStructures;
+
+namespace AdventOfCode2025
+{
+    public class Day9 : IDay
+    {
+        public string InputPath { get; set; } = "day9Input.txt";
+        //public string InputPath { get; set; } = "day9InputTest.txt";
+
+        public void PartOne()
+        {
+            long[][] s = File.ReadAllLines(InputPath)
+                .Select(x => x.Split(",")
+                    .Select(long.Parse)
+                    .ToArray())
+                .ToArray();
+
+            long solution = 1;
+
+            for (int i = 0; i < s.Length; i++)
+                for (int j = i + 1; j < s.Length; j++)
+                    solution = Math.Max(solution, (Math.Abs(s[i][0] - s[j][0]) + 1) * (Math.Abs(s[i][1] - s[j][1]) + 1));
+
+            // 4760959496
+            Console.WriteLine($"The largest area of any rectangle you can make is {solution}");
+        }
+
+        public void PartTwo()
+        {
+            long[][] s = File.ReadAllLines(InputPath)
+                .Select(x => x.Split(",")
+                    .Select(long.Parse)
+                    .ToArray())
+                .ToArray();
+
+            List<Day9Line> lines = new();
+
+            long solution = 1;
+
+            for (int i = 1; i < s.Length; i++)
+                lines.Add(new Day9Line(s[i - 1], s[i]));
+
+            // closing the loop
+            lines.Add(new Day9Line(s[^1], s[0]));
+
+            for (int i = 0; i < s.Length; i++)
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (lines.Any(x => x.IntersectsWithRect(s[i], s[j])))
+                        continue;
+
+                    long area = (Math.Abs(s[i][0] - s[j][0]) + 1) * (Math.Abs(s[i][1] - s[j][1]) + 1);
+                    solution = Math.Max(solution, area);
+                }
+
+            // 1343576598
+            Console.WriteLine($"The largest area of any rectangle you can make is {solution}");
+        }
+    }
+}
---
+++ AdventOfCode2025/DataStructures/Day9Line.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Day9Line
+    {
+        // the left point when horizontal, the bottom point when vertical
+        long[] StartCoords { get; set; }
+        long[] EndCoords { get; set; }
+        public Day9Line(long[] startCoords, long[] endCoords)
+        {
+            // Wrong order
+            if (endCoords[0] < startCoords[0] // Horizontal line
+                || endCoords[1] < startCoords[1]) // Vertical line
+            {
+                StartCoords = endCoords;
+                EndCoords = startCoords;
+                return;
+            }
+
+            // Arrived in the correct order
+            StartCoords = startCoords;
+            EndCoords = endCoords;
+        }
+
+        // Doesn't check for edge collisions
+        public bool IntersectsWithRect(long[] rectStart, long[] rectEnd)
+        {
+            long minX = Math.Min(rectStart[0], rectEnd[0]);
+            long maxX = Math.Max(rectStart[0], rectEnd[0]);
+            long minY = Math.Min(rectStart[1], rectEnd[1]);
+            long maxY = Math.Max(rectStart[1], rectEnd[1]);
+
+            long[] bottomLeftPoint = [minX, minY];
+            long[] topRightPoint = [maxX, maxY];
+
+            bool xIntersects = minX < StartCoords[0] && StartCoords[0] < maxX
+                    || minX < EndCoords[0] && EndCoords[0] < maxX
+                    || StartCoords[0] < minX && maxX <= EndCoords[0]
+                    || StartCoords[0] <= minX && maxX < EndCoords[0];
+
+            bool yIntersects = minY < StartCoords[1] && StartCoords[1] < maxY
+                    || minY < EndCoords[1] && EndCoords[1] < maxY
+                    || StartCoords[1] < minY && maxY <= EndCoords[1]
+                    || StartCoords[1] <= minY && maxY < EndCoords[1];
+
+            return xIntersects && yIntersects;
+        }
+    }
+}
